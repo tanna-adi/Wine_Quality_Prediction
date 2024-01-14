000641be79708856a01fa11a292e6dab2f421538
tanna-adi <adityatanna99@gmail.com>
2024-01-14 18:37:10 +0530

    update

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 from Wine_Quality_Prediction import logger
+from Wine_Quality_Prediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e

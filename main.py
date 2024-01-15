ad34b9f6af77970f18d675cfbe820ebd7826bc7a
tanna-adi <adityatanna99@gmail.com>
2024-01-15 22:38:40 +0530

    model evaluation added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Wine_Quality_Prediction.pipeline.stage_01_data_ingestion import DataIngesti
 from Wine_Quality_Prediction.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from Wine_Quality_Prediction.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from Wine_Quality_Prediction.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from Wine_Quality_Prediction.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -40,6 +41,17 @@ STAGE_NAME = "Model Trainer stage"
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = ModelEvaluationTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
 except Exception as e:
